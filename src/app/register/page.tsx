4fcc56eddb42c039b80e67e0f6df74621af41de6
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-08-15 19:09:46 -0300

    chore(register-page): integrated register page to react hook form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import Link from 'next/link'
+
+import { useRegisterForm } from '@hooks/forms/useRegisterForm'
 
 import { Button } from '@fragments/Button'
 import { Input } from '@fragments/Input'
 
 export default function Register() {
+  const { register, handleSubmit, errors } = useRegisterForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex w-full flex-col bg-gradient-to-r  from-zinc-900 to-zinc-950">
       <main className="m-auto flex min-h-screen w-full max-w-[80rem] items-center justify-center px-4 py-24">
@@ -12,18 +22,25 @@ export default function Register() {
             Crie a sua conta
           </h2>
 
-          <form className="flex w-full flex-col gap-4 rounded-md border-[1px] border-red-500 bg-zinc-800 p-8">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex w-full flex-col gap-4 rounded-md border-[1px] border-red-500 bg-zinc-800 p-8"
+          >
             <div className="flex flex-col gap-4">
               <Input
-                labelText="Seu nome"
-                id="name-field"
-                placeholder="Irmão do forró"
+                labelText="Seu apelido"
+                id="username-field"
+                placeholder="irmao_do_forro"
+                errorText={errors.username?.message}
+                {...register('username')}
               />
 
               <Input
                 labelText="Seu e-mail"
                 id="email-field"
                 placeholder="irmaosdoforro@gmail.com"
+                errorText={errors.email?.message}
+                {...register('email')}
               />
 
               <Input
@@ -31,6 +48,8 @@ export default function Register() {
                 labelText="Sua senha"
                 id="password-field"
                 placeholder="AbubLeble1234598!@#"
+                errorText={errors.password?.message}
+                {...register('password')}
               />
 
               <Input
@@ -38,11 +57,13 @@ export default function Register() {
                 labelText="Confirme a sua senha"
                 id="confirm-password-field"
                 placeholder="AbubLeble1234598!@#"
+                errorText={errors.confirmPassword?.message}
+                {...register('confirmPassword')}
               />
             </div>
 
             <div className="flex flex-col items-start justify-center gap-4">
-              <Button>Entrar</Button>
+              <Button type="submit">Começar a assistir animes</Button>
               <Link href="/" className="text-md text-gray-100 hover:opacity-80">
                 Já possuo uma conta, quero logar!
               </Link>
--- src/hooks/forms/useRegisterForm.tsx
+++ src/hooks/forms/useRegisterForm.tsx
@@ -3,22 +3,42 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 type TRegisterForm = {
-  name: string
+  username: string
   email: string
   password: string
   confirmPassword: string
 }
 
 export const useRegisterForm = () => {
-  const registerFormSchema = z.object({
-    email: z
-      .string()
-      .regex(
-        /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-        'Digite um email válido',
-      ),
-    password: z.string(),
-  })
+  const registerFormSchema = z
+    .object({
+      username: z
+        .string()
+        .nonempty('Esse campo é obrigatório')
+        .min(4, 'O username precisa ter no mínimo 4 caracteres')
+        .max(15, 'O username pode ter no máximo 15 caracteres')
+        .regex(/^[a-z]+$/, 'O username só pode ter letras minúsculas'),
+      email: z
+        .string()
+        .nonempty('Esse campo é obrigatório')
+        .regex(
+          /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+          'Digite um email válido',
+        ),
+      password: z
+        .string()
+        .nonempty('Esse campo é obrigatório')
+        .min(8, 'A senha precisa ter no mínimo 8 dígitos')
+        .regex(
+          /^(?=.*[A-Za-z])(?=.*\d).+$/,
+          'A senha deve ter letras e números',
+        ),
+      confirmPassword: z.string().nonempty('Esse campo é obrigatório'),
+    })
+    .refine(({ password, confirmPassword }) => password === confirmPassword, {
+      message: 'As senhas precisam ser iguais',
+      path: ['confirmPassword'],
+    })
 
   const {
     register,
