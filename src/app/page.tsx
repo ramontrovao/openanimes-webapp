f730b8c9db93a70b93eef14a9b06bf1fce6d8f02
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-08-16 22:07:44 -0300

    feat(api-integration): finished login api integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,75 @@
 'use client'
 
 import Link from 'next/link'
+import { toast, ToastContainer } from 'react-toastify'
+
+import { loginUser } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 import { Input } from '@fragments/Input'
 import { Button } from '@fragments/Button'
 
 import { useLoginForm } from '@hooks/forms/useLoginForm'
 
+type TLoginForm = {
+  email: string
+  password: string
+}
+
 export default function Home() {
   const { register, handleSubmit, errors } = useLoginForm()
 
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = async ({ email, password }: TLoginForm) => {
+    try {
+      const payload = { email, password }
+      const JWTtoken = await loginUser(payload)
+
+      console.log(JWTtoken)
+      return toast.success('Autenticado com sucesso =D', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      })
+    } catch (error) {
+      if (error instanceof AppError) {
+        return toast.error(error.message, {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        })
+      }
+
+      return console.log(error)
+    }
   }
 
   return (
     <div className="flex w-full flex-col bg-gradient-to-r  from-zinc-900 to-zinc-950">
       <main className="m-auto flex min-h-screen w-full max-w-[80rem] items-center justify-center px-4 py-24">
         <section className="flex w-full max-w-[40rem] flex-col gap-8">
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+
           <h2 className="text-center text-2xl text-gray-100">
             Acesse a sua conta
           </h2>
