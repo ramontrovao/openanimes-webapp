fa30b4a534c2f9b199e40038f676b52e1b044207
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-08-16 21:57:21 -0300

    feat(api-integration): finished api integration in register screen

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 import { TRegisterUser } from '@/src/types/User'
+import { AppError } from '@utils/AppError'
+import { AnyAaaaRecord } from 'dns'
 
 const api = axios.create({
   baseURL: 'https://api.learxd.dev/open-animes/v1',
@@ -21,7 +23,16 @@ export const registerUser = async ({
     })
 
     return res
-  } catch (error) {
+  } catch (error: any) {
+    const errorMessage = error.response.data.message as string
+    const errorStatusCode = error.response.status as number
+
+    console.log(errorMessage, errorStatusCode)
+
+    if (errorMessage && errorStatusCode) {
+      throw new AppError(errorMessage, errorStatusCode)
+    }
+
     throw error
   }
 }
